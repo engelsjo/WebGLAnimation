543b522021ba26e3dfd6ce3754759e48a575888f
Josh <engelsjo@mail.gvsu.edu>
2015-03-28 13:06:42 -0400

    bug fixes to cube

--- src/shapes/Cube.js
+++ src/shapes/Cube.js
@@ -33,57 +33,57 @@ var Cube = function(length, width, height, material){
     geometry.addAttribute('position', new THREE.BufferAttribute(vertexArr, 3));
 
     //adding the normals the hard and brutal way
-    vertexArr[0] = -1 / Math.sqrt(3);
-    vertexArr[1] = -1 / Math.sqrt(3);
-    vertexArr[2] = 1 / Math.sqrt(3);
-    vertexArr[3] = 1 / Math.sqrt(3);
-    vertexArr[4] = -1 / Math.sqrt(3);
-    vertexArr[5] = 1 / Math.sqrt(3);
-    vertexArr[6] = 1 / Math.sqrt(3);
-    vertexArr[7] = 1 / Math.sqrt(3);
-    vertexArr[8] = 1 / Math.sqrt(3);
-    vertexArr[9] = -1 / Math.sqrt(3);
-    vertexArr[10] = 1 / Math.sqrt(3);
-    vertexArr[11] = 1 / Math.sqrt(3);
-    vertexArr[12] = -1 / Math.sqrt(3);
-    vertexArr[13] = 1 / Math.sqrt(3);
-    vertexArr[14] = -1 / Math.sqrt(3);
-    vertexArr[15] = 1 / Math.sqrt(3);
-    vertexArr[16] = 1 / Math.sqrt(3);
-    vertexArr[17] = -1 / Math.sqrt(3);
-    vertexArr[18] = 1 / Math.sqrt(3);
-    vertexArr[19] = -1 / Math.sqrt(3);
-    vertexArr[20] = -1 / Math.sqrt(3);
-    vertexArr[21] = -1 / Math.sqrt(3);
-    vertexArr[22] = -1 / Math.sqrt(3);
-    vertexArr[23] = -1 / Math.sqrt(3);
+    normalArr[0] = -1 / Math.sqrt(3);
+    normalArr[1] = -1 / Math.sqrt(3);
+    normalArr[2] = 1 / Math.sqrt(3);
+    normalArr[3] = 1 / Math.sqrt(3);
+    normalArr[4] = -1 / Math.sqrt(3);
+    normalArr[5] = 1 / Math.sqrt(3);
+    normalArr[6] = 1 / Math.sqrt(3);
+    normalArr[7] = 1 / Math.sqrt(3);
+    normalArr[8] = 1 / Math.sqrt(3);
+    normalArr[9] = -1 / Math.sqrt(3);
+    normalArr[10] = 1 / Math.sqrt(3);
+    normalArr[11] = 1 / Math.sqrt(3);
+    normalArr[12] = -1 / Math.sqrt(3);
+    normalArr[13] = 1 / Math.sqrt(3);
+    normalArr[14] = -1 / Math.sqrt(3);
+    normalArr[15] = 1 / Math.sqrt(3);
+    normalArr[16] = 1 / Math.sqrt(3);
+    normalArr[17] = -1 / Math.sqrt(3);
+    normalArr[18] = 1 / Math.sqrt(3);
+    normalArr[19] = -1 / Math.sqrt(3);
+    normalArr[20] = -1 / Math.sqrt(3);
+    normalArr[21] = -1 / Math.sqrt(3);
+    normalArr[22] = -1 / Math.sqrt(3);
+    normalArr[23] = -1 / Math.sqrt(3);
     geometry.addAttribute('normal', new THREE.BufferAttribute(normalArr, 3));
 
     //add the indices..
     for(var k = 0; k < 4; k++){ //front face
-    	indexArr[k] = k;
+    	indexArr[k] = k * 3;
     }
 
     for (var k = 4; k < 8; k++){ //back face
-    	indexArr[k] = k;
+    	indexArr[k] = k * 3;
     }
 
-    var top_index = [5, 4, 3, 2];
+    var top_index = [15, 12, 9, 6];
     for (var k = 0; k < 4; k++){ //top face
     	indexArr[k + 8] = top_index[k]
     }
 
-    var bott_index = [1, 0, 7, 6];
+    var bott_index = [3, 0, 21, 18];
     for (var k = 0; k < 4; k++){ //bottom face
     	indexArr[k + 12] = bott_index[k]
     }
 
-    var right_index = [2, 1, 6, 5];
+    var right_index = [6, 3, 18, 15];
     for (var k = 0; k < 4; k++){ //right face
     	indexArr[k + 16] = right_index[k]
     }
 
-    var left_index = [7, 0, 3, 4];
+    var left_index = [21, 0, 9, 12];
     for (var k = 0; k < 4; k++){ //left face
     	indexArr[k + 20] = left_index[k]
     }
