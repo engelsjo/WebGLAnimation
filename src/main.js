b1d925d2fc5174cec0125a58f071858c7790ad63
Josh <engelsjo@mail.gvsu.edu>
2015-03-31 22:23:43 -0400

    added the helicopter model / base animation

--- src/main.js
+++ src/main.js
@@ -16,12 +16,13 @@ require([], function(){
 	// setup a scene and camera
 	var scene	= new THREE.Scene();
 	var camera	= new THREE.PerspectiveCamera(60, CANVAS_WIDTH / CANVAS_HEIGHT, 0.01, 1000);
-//    camera.up.set (0, 0, 1); /* use the Z axis as the upright direction */
-//    camera.position.x = 3;
     camera.position.y = 25;
 	camera.position.z = 30;
 
-//    scene.add (new THREE.GridHelper(10, 1));
+    //declare all global variables
+    var auto_pilot = true;
+    var rear_rpm = 0;
+
 	// declare the rendering loop
 	var onRenderFcts= [];
 
@@ -45,7 +46,7 @@ require([], function(){
     scene.add ( new THREE.SpotLightHelper (backLight, 0.2));
 
     // TEXTURE setup
-    var path = "textures/MAK/"
+    //var path = "textures/MAK/" TODO: work here
     /* The image names can be ANYTHING, but the the order of the SIX images
        in the array will be used as follows:
        the 1st image => Positive X axis
@@ -56,47 +57,59 @@ require([], function(){
        the 6th image => Negative Z axis
      */
 
+    /* TODO: work here
     var images = [path + "posx.png", path + "negx.png",
         path + "posy.png", path + "negy.png",
         path + "posz.png", path + "negz.png"];
-
-    var cubemap = THREE.ImageUtils.loadTextureCube( images );
+        */
+    //var cubemap = THREE.ImageUtils.loadTextureCube( images );
 	//////////////////////////////////////////////////////////////////////////////////
 	//		add an object and make it move					//
 	//////////////////////////////////////////////////////////////////////////////////	
-    var wheel_cf = new THREE.Matrix4();
-    wheel_cf.makeTranslation(0, -20, 0);
-    var arm_cf = new THREE.Matrix4();
-    arm_cf.makeRotationZ(THREE.Math.degToRad(40));
-    var wheel = new Wheel();
-    var arm = new SwingArm(20);
-//    arm.add (new THREE.AxisHelper(2));
-    var frame = new SwingFrame();
-    arm.add (wheel);
-    frame.add(arm);
-    scene.add (frame);
+    //add the helicopter cfs
+    var helibase_cf = new THREE.Matrix4();
+    var heli_blade_cf = new THREE.Matrix4();
+    var heli_rear_cf = new THREE.Matrix4();
+
+    //set the initial place of the helicopter
+    helibase_cf.makeTranslation(0, 75, 40);
+    heli_blade.multiply(new THREE.Matrix4().makeTranslation(0, 0, 5.75), new THREE.Matrix4().makeRotationZ(THREE.Math.degToRad(45)));
+    heli_rear_cf.multiply(new THREE.Matrix4().makeTranslation(-11, -1.2, 3.75), new THREE.Matrix4().scale(new Vector3(.25, .25, .25)), new THREE.Matrix4().makeRotationX(THREE.Math.degToRad(90)));
+    
+    //init the models for the helicopter
+    var helibase = new HeliBase();
+    var top_blade = new Blade();
+    var rear_blade = new Blade();
+
+    helibase.add(top_blade);
+    helibase.add(rear_blade);
+    scene.add(helibase);
     scene.add (new THREE.AxisHelper(4));
 
     /* Load the first texture image */
-    var stone_tex = THREE.ImageUtils.loadTexture("textures/stone256.jpg");
+    //TODO: work here
+    //var stone_tex = THREE.ImageUtils.loadTexture("textures/stone256.jpg");
     /* for repeat to work, the image size must be 2^k */
 
     /* repeat the texture 4 times in both direction */
-    stone_tex.repeat.set(4,4);
-    stone_tex.wrapS = THREE.RepeatWrapping;
-    stone_tex.wrapT = THREE.RepeatWrapping;
+    //TODO work here
+    //stone_tex.repeat.set(4,4);
+    //stone_tex.wrapS = THREE.RepeatWrapping;
+    //stone_tex.wrapT = THREE.RepeatWrapping;
 
     /* Load the second texture image */
-    var wood_tex = THREE.ImageUtils.loadTexture("textures/wood256.jpg");
+    //TODO
+    //var wood_tex = THREE.ImageUtils.loadTexture("textures/wood256.jpg");
 
     /* mirror repeat the texture 2 times, without
      * mirror repeat the seam between the left
      * and right edge of the texture will be
      * visible */
-    wood_tex.repeat.set(2,2);
-    wood_tex.wrapS = THREE.MirroredRepeatWrapping;
-    wood_tex.wrapT = THREE.MirroredRepeatWrapping;
-    var groundPlane = new THREE.PlaneBufferGeometry(40, 40, 10, 10);
+     //TODO
+    //wood_tex.repeat.set(2,2);
+    //wood_tex.wrapS = THREE.MirroredRepeatWrapping;
+    //wood_tex.wrapT = THREE.MirroredRepeatWrapping;
+    var groundPlane = new THREE.PlaneBufferGeometry(150, 150, 10, 10);
     /* attach the texture as the "map" property of the material */
     var groundMat = new THREE.MeshPhongMaterial({color:0x1d6438, ambient:0x1d6438, map:stone_tex});
     var ground = new THREE.Mesh (groundPlane, groundMat);
@@ -105,41 +118,39 @@ require([], function(){
 
     var sphereGeo = new THREE.SphereGeometry(8, 30, 20);
     /* attach the texture as the "map" property of the material */
-    var sphereMat = new THREE.MeshBasicMaterial ({envMap:cubemap});
-    var sphere = new THREE.Mesh (sphereGeo, sphereMat);
-    sphere.position.x = 10;
-    sphere.position.y = 10;
-    sphere.position.z = 10;
+    //var sphereMat = new THREE.MeshBasicMaterial ({envMap:cubemap});
+    //var sphere = new THREE.Mesh (sphereGeo, sphereMat);
+    //sphere.position.x = 10;
+    //sphere.position.y = 10;
+    //sphere.position.z = 10;
     scene.add(sphere);
-//    var grid = new THREE.GridHelper(50, 1);
-//    scene.add (grid);
-
-//  var myCone = new Cone(40);
-//  var coneMat = new THREE.MeshPhongMaterial({color:0x0f0650});
-//  scene.add(new THREE.Mesh(myCone, coneMat));
 
     camera.lookAt(new THREE.Vector3(0, 5, 0));
-//    mesh.matrixAutoUpdate = false;
 
 	onRenderFcts.push(function(delta, now){
         if (pauseAnim) return;
-        var tran = new THREE.Vector3();
-        var quat = new THREE.Quaternion();
-        var rot = new THREE.Quaternion();
-        var vscale = new THREE.Vector3();
+        if (auto_pilot){
+            var rps = .125;
+            rear_rpm = 0;
+            var rotations = rps * delta / 1000;
+            var delta_degrees = -(rotations * 360);
 
-        wheel_cf.multiply(new THREE.Matrix4().makeRotationZ(THREE.Math.degToRad(delta * 72)));
-        wheel_cf.decompose(tran, quat, vscale);
-        wheel.position.copy(tran);
-        wheel.quaternion.copy(quat);
+            var tran = new THREE.Vector3();
+            var quat = new THREE.Quaternion();
+            var rot = new THREE.Quaternion();
+            var vscale = new THREE.Vector3();
 
-        /* TODO: when animation is resumed after a pause, the arm jumps */
-        var curr_angle = 40.0 * Math.cos(now);
-        arm_cf.copy(new THREE.Matrix4().makeRotationZ(THREE.Math.degToRad(curr_angle)));
-        arm_cf.decompose (tran, quat, vscale);
-//        rot.setFromAxisAngle( new THREE.Vector3(0,0,1), THREE.Math.degToRad(arm_angle));
-        arm.position.copy(tran);
-        arm.quaternion.copy(quat);
+            helibase_cf.copy(new THREE.Matrix4().makeRotationZ(THREE.Math.degToRad(delta_degrees)).multiply(helibase_cf));
+            helibase_cf.decompose(tran, quat, vscale);
+            helibase.position.copy(tran);
+            helibase.quaternion.copy(quat);
+
+
+            //wheel_cf.multiply(new THREE.Matrix4().makeRotationZ(THREE.Math.degToRad(delta * 72)));
+            //wheel_cf.decompose(tran, quat, vscale);
+            //wheel.position.copy(tran);
+            //wheel.quaternion.copy(quat);
+        }
 	});
 	
 	//////////////////////////////////////////////////////////////////////////////////
